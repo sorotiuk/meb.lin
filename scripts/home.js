81277883e88897f632578120a7d9a7eea79d0460
Tania Sorotiuk <tsorotiuk@gmail.com>
2017-04-28 12:51:34 +0300

    task 07: added animate

--- scripts/home.js
+++ scripts/home.js
@@ -3,3 +3,26 @@
  */
 
 console.log("start js logic");
+
+
+$("#myCarousel").on("slide.bs.carousel", function () {
+    $( ".hero-title-description" ).animate({
+        opacity: 0,
+        marginTop: 300,
+    }, 400).delay(300);
+    $( ".hero-title" ).animate({
+        opacity: 0,
+        marginLeft: 300,
+    }, 600);
+})
+
+$("#myCarousel").on("slid.bs.carousel", function () {
+    $( ".hero-title" ).animate({
+        opacity: 1,
+        marginLeft: 0,
+    }, 400);
+    $( ".hero-title-description" ).animate({
+        opacity: 1,
+        marginTop: 0,
+    }, 600);
+})
